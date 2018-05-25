370c233994dcdf684a125a194b592f473782e3ad
KZACHAR <KAMIL.ZACHAREWICZ@GMAIL.COM>
2018-05-25 17:20:01 +0200

    post, get, delete, update

--- src/main/java/pl/coderstrust/accounting/controller/InvoiceController.java
+++ src/main/java/pl/coderstrust/accounting/controller/InvoiceController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,35 +15,41 @@ import pl.coderstrust.accounting.model.validator.CompanyValidator;
 import pl.coderstrust.accounting.model.validator.InvoiceEntryValidator;
 import pl.coderstrust.accounting.model.validator.InvoiceValidator;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Collection;
 
 @RestController
-@RequestMapping
+@RequestMapping("/invoices")
 public class InvoiceController {
 
   private InvoiceService invoiceService = new InvoiceService(new InMemoryDatabase(),
       new InvoiceValidator(new InvoiceEntryValidator(), new CompanyValidator()));
 
-  @GetMapping("/invoices")
+  @GetMapping
   public Collection<Invoice> findInvoices() {
     return invoiceService
-        .findInvoices(new Invoice(null, null, null,null, null, null), null, null);
+        .findInvoices(null, LocalDate.of(2018, Month.JANUARY, 1),LocalDate.of(2018, Month.JANUARY, 6));
   }
 
-  @GetMapping("/invoices/{id}")
-  public Collection<Invoice> findInvoice(@PathVariable(name="id", required = true) int id) {
+  @GetMapping("/{id}")
+  public Collection<Invoice> findSingleIvoiceById(@PathVariable(name="id", required = true) int id) {
     return invoiceService
         .findInvoices(new Invoice(id, null, null,null, null, null), null, null);
   }
 
-  @PostMapping("/invoices")
+  @PostMapping
   public int saveInvoice(@RequestBody Invoice invoice) {
     return invoiceService.saveInvoice(invoice);
   }
 
-  @DeleteMapping("/invoices")
+  @DeleteMapping
   public void removeInvoice(@RequestBody int id) {
     invoiceService.removeInvoice(id);
   }
 
+  @PutMapping
+  public void updateInvoice(@RequestBody Invoice invoice) {
+    invoiceService.updateInvoice(invoice);
+  }
 }
