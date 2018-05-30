8348ea8f626469149dc6406ef7244f2e939bd582
AbAjDaLski <bajdalski.a@protonmail.com>
2018-05-30 19:09:26 +0200

    Starting implement method finde.

--- src/main/java/pl/coderstrust/accounting/database/impl/file/InFileDatabase.java
+++ src/main/java/pl/coderstrust/accounting/database/impl/file/InFileDatabase.java
@@ -3,15 +3,24 @@ package pl.coderstrust.accounting.database.impl.file;
 import pl.coderstrust.accounting.database.Database;
 import pl.coderstrust.accounting.helpers.FileInvoiceHelper;
 import pl.coderstrust.accounting.model.Invoice;
+import pl.coderstrust.accounting.model.InvoiceEntry;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class InFileDatabase implements Database {
 
   private String databaseFilePath;
   private String idFilePath;
+
+  private final Map<Integer, Invoice> invoices = new HashMap<>();
+
+  private int id = 0;
 
   public InFileDatabase(String databaseFilePath, String idFilePath) {
     if (databaseFilePath == null || "".equals(databaseFilePath)) {
@@ -58,7 +67,68 @@ public class InFileDatabase implements Database {
   @Override
   public Collection<Invoice> find(Invoice searchParams, LocalDate issuedDateFrom,
       LocalDate issuedDateTo) {
-    return null;
+    Set<Invoice> resultSchearching = new HashSet<>();
+    // kolekcja przechowująca indywidualne nie powtarzające się elementy
+
+    //wczytanie wszystkich faktur z pliku przesukanie ich wg
+//resultSchearching.addAll()
+    invoices = FileInvoiceHelper.readInvoicesFromFile(databaseFilePath);
+//
+    for (Invoice invoice : invoices.values()) {
+      if (searchParams != null) {// faktur z konkretnym parametrem.
+        if (searchParams.getId() != null) {
+          if (searchParams.getId().equals(invoice.getId())) {
+            resultSchearching.add(invoice);
+            //zapisanie do pliku
+
+            break;
+          }
+        }
+        if (searchParams.getIdentifier() != null) {
+          if (searchParams.getIdentifier().equals(invoice.getIdentifier())) {
+            resultSchearching.add(invoice);
+            break;
+          }
+        }
+        if (searchParams.getBuyer() != null) {
+          if (searchParams.getBuyer().equals(invoice.getBuyer())) {
+            resultSchearching.add(invoice);
+            break;
+          }
+        }
+        if (searchParams.getSeller() != null) {
+          if (searchParams.getSeller().equals(invoice.getSeller())) {
+            resultSchearching.add(invoice);
+            break;
+          }
+        }
+        if (searchParams.getIssuedDate() != null) {
+          if (searchParams.getIssuedDate().isEqual(invoice.getIssuedDate())) {
+            resultSchearching.add(invoice);
+            break;
+          }
+        }
+
+        //czytanie wszystkich faktur
+      }
+      if (searchParams.getEntries() != null) {
+        boolean found = false;
+        for (InvoiceEntry entry : searchParams.getEntries()) {
+          if (!found && invoice.getEntries().contains(entry)) {
+            resultSchearching.add(invoice);
+            found = true;
+          }
+        }
+      }
+      //szukanie faktur od jednej daty do 2 daty - z przedzoału
+      //szukanie faktur od jednej daty do 2 daty - z przedzoału
+      //szuanie faktur od daty do końca faktur
+      //szukanie faktur do daty
+
+    }
+    return resultSchearching;
+
+
   }
 
   @Override
