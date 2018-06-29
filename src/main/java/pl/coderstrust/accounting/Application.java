6138cd45fc365ed2291e2e3965090e879768687a
KZACHAR <KAMIL.ZACHAREWICZ@GMAIL.COM>
2018-06-29 19:54:01 +0200

    logger added

--- src/main/java/pl/coderstrust/accounting/Application.java
+++ src/main/java/pl/coderstrust/accounting/Application.java
@@ -1,12 +1,24 @@
 package pl.coderstrust.accounting;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+ private static Logger logger = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+
+    try {
+      throw new NullPointerException();
+    }catch (NullPointerException ex){
+      logger.warn("test", ex);
+      throw new RuntimeException(ex);
+    }
   }
+
 }
