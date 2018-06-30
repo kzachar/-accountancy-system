ff21e52055cbd9d2b1d6a9dca0d8b1ee6dac3a3a
KZACHAR <KAMIL.ZACHAREWICZ@GMAIL.COM>
2018-06-30 10:40:44 +0200

    login to file added

---
+++ src/main/java/RequestLoggingFilterConfig.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        return loggingFilter;
+    }
+}
--- src/main/java/pl/coderstrust/accounting/Application.java
+++ src/main/java/pl/coderstrust/accounting/Application.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
 
@@ -13,12 +14,16 @@ public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
 
-    try {
-      throw new NullPointerException();
-    }catch (NullPointerException ex){
-      logger.warn("test", ex);
-      throw new RuntimeException(ex);
-    }
+
+
+//    try {
+//      throw new NullPointerException();
+//    }catch (NullPointerException ex){
+//      logger.warn("test", ex);
+//      throw new RuntimeException(ex);
+//    }
+
+
   }
 
 }
