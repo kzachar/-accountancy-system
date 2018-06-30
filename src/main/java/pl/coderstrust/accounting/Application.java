882d734c14ab922f14f3d595f89d035bc521d31b
KZACHAR <KAMIL.ZACHAREWICZ@GMAIL.COM>
2018-06-30 11:17:12 +0200

    CommonsRequestLoggingFilter added

--- src/main/java/pl/coderstrust/accounting/Application.java
+++ src/main/java/pl/coderstrust/accounting/Application.java
@@ -4,12 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 
 @SpringBootApplication
 public class Application {
 
  private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setIncludeHeaders(false);
+    return loggingFilter;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
